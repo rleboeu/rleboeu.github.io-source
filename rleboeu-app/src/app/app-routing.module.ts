8448bc6f70913ca7c856d036cb352d833168a6cc
rleboeu <leboeufrm@gmail.com>
2023-03-06 22:44:05 -0500

    added components, not final

--- rleboeu-app/src/app/app-routing.module.ts
+++ rleboeu-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamingComponent } from './components/gaming/gaming.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'gaming', component: GamingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rleboeu-app/src/app/app.module.ts
+++ rleboeu-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GamingComponent } from './components/gaming/gaming.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GamingComponent,
+    ProjectsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
