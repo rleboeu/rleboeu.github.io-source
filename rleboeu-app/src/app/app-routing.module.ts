dc7eca4f77dfb6af4099e2536387632f63a7cf45
rleboeu <leboeufrm@gmail.com>
2023-05-02 15:53:24 -0400

    starting project over, delete components and add notification to home screen

--- rleboeu-app/src/app/app-routing.module.ts
+++ rleboeu-app/src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BioComponent } from './components/bio/bio.component';
-import { GamingComponent } from './components/gaming/gaming.component';
 import { HomeComponent } from './components/home/home.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ResumeComponent } from './components/resume/resume.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'gaming', component: GamingComponent },
-  { path: 'bio', component: BioComponent },
-  { path: 'resume', component: ResumeComponent }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
--- rleboeu-app/src/app/app.module.ts
+++ rleboeu-app/src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { GamingComponent } from './components/gaming/gaming.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { BioComponent } from './components/bio/bio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    GamingComponent,
-    ProjectsComponent,
-    PortfolioComponent,
-    ResumeComponent,
-    BioComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
